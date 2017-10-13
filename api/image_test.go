82a9b6fe95e1e64339c9fc235fc132b96e122f0b
komu <komuw05@gmail.com>
2017-10-13 15:23:26 +0300

    test image pull

--- api/image_test.go
+++ api/image_test.go
@@ -16,3 +16,22 @@ func BenchmarkPullDockerImage(b *testing.B) {
 		b.Log(err)
 	}
 }
+
+func TestGetPullDockerImage(t *testing.T) {
+	tt := []struct {
+		input       string
+		expectedErr error
+	}{
+		{"busybox", nil},
+	}
+	var ctx = context.Background()
+	var cli, _ = client.NewEnvClient()
+	defer cli.Close()
+
+	for _, v := range tt {
+		err := PullDockerImage(ctx, v.input, cli)
+		if err != nil {
+			t.Errorf("\nran PullDockerImage(%#+v) \ngot %s \nwanted %#+v", v.input, err, v.expectedErr)
+		}
+	}
+}
