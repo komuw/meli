d0fbaea718952cf5285ccc47b2be5b9c3e2a0b2e
komu <komuw05@gmail.com>
2017-10-14 14:15:13 +0300

    test format ports

--- api/format_test.go
+++ api/format_test.go
@@ -21,16 +21,37 @@ func TestFormatLabels(t *testing.T) {
 	}
 }
 
+func TestFormatPorts(t *testing.T) {
+	tt := []struct {
+		input    string
+		expected []string
+	}{
+		{"6300:6379", []string{"6300", "6379"}},
+	}
+	for _, v := range tt {
+		actual := FormatPorts(v.input)
+		if !reflect.DeepEqual(actual, v.expected) {
+			t.Errorf("\nran FormatLabels(%#+v) \ngot %#+v \nwanted %#+v", v.input, actual, v.expected)
+		}
+	}
+}
+
 func BenchmarkFormatLabels(b *testing.B) {
 	// run the FormatLabels function b.N times
 	for n := 0; n < b.N; n++ {
-		FormatLabels("traefik.backend=web")
+		_ = FormatLabels("traefik.backend=web")
+	}
+}
+
+func BenchmarkFormatPorts(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		_ = FormatPorts("6300:6379")
 	}
 }
 
 func BenchmarkFormatContainerName(b *testing.B) {
 	for n := 0; n < b.N; n++ {
-		FormatContainerName("build_with_no_specified_dockerfile")
+		_ = FormatContainerName("build_with_no_specified_dockerfile")
 	}
 
 }
