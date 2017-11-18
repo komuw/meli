85e844f034e0fd3e89a92e904932d236b8fa6930
komuW <komuw05@gmail.com>
2017-11-18 21:00:04 +0300

    meli version v0.1.1.4

--- HomebrewFormula/meli.rb
+++ HomebrewFormula/meli.rb
@@ -2,9 +2,9 @@ class Meli < Formula
   desc "Meli is supposed to be a faster alternative to docker-compose.
  Faster in the sense that, Meli will try to pull as many services(docker containers) as it can in parallel."
   homepage "https://github.com/komuW/meli"
-  url "https://github.com/komuW/meli/releases/download/v0.1.1.3/meli_0.1.1.3_darwin_amd64.tar.gz"
-  version "0.1.1.3"
-  sha256 "dfe0f33a3e1f4ed95cc43c7461ef92a68fc7272fda26fa961b73f1d1f4f150ce"
+  url "https://github.com/komuW/meli/releases/download/v0.1.1.4/meli_0.1.1.4_darwin_amd64.tar.gz"
+  version "0.1.1.4"
+  sha256 "8936aa76c2665180140e24ac309cb17caf70c62d211aaa81dc20bf3959e9380b"
 
   def install
     bin.install "program"
