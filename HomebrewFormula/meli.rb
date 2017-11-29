01eb02f7fe3072445ea8ce8231afac26cf216160
komuW <komuw05@gmail.com>
2017-11-29 18:11:48 +0300

    strict brew

--- HomebrewFormula/meli.rb
+++ HomebrewFormula/meli.rb
@@ -1,6 +1,5 @@
 class Meli < Formula
-  desc "Meli is supposed to be a faster alternative to docker-compose.
- Faster in the sense that, Meli will try to pull as many services(docker containers) as it can in parallel."
+  desc "Faster alternative to docker-compose"
   homepage "https://github.com/komuW/meli"
   url "https://github.com/komuW/meli/releases/download/v0.1.8/meli_0.1.8_darwin_amd64.tar.gz"
   version "0.1.8"
@@ -11,6 +10,6 @@ class Meli < Formula
   end
 
   test do
-    system "#{bin}/meli --version"
+    system "#{bin}/meli", "--version"
   end
 end
