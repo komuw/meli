02c7a12cc59a5e9ee23e321fc1209658e50ea373
Elan Ruusamäe <glen@pld-linux.org>
2018-02-27 18:31:48 +0200

    install.sh: fix typo (#94)

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ usage() {
 $this: download go binaries for komuw/meli
 
 Usage: $this [-b] bindir [version]
-  -b sets bindir or installation directory, default "/usr/local/bin}"
+  -b sets bindir or installation directory, default "/usr/local/bin"
    [version] is a version number from
    https://github.com/komuw/meli/releases
    If version is missing, then an attempt to find the latest will be found.
